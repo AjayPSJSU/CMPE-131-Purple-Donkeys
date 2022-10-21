5db43c6f6753d0ad5675dee514d7e0685caa4c95
Jarvis Lam <102021121+JarvisLam-LemonCEO@users.noreply.github.com>
2022-10-21 14:59:48 -0700

    Update Login.js
    I have made some changes to the Login page

--- chat-bot/front-end/src/pages/Login.js
+++ chat-bot/front-end/src/pages/Login.js
@@ -1,6 +1,7 @@
 import { Form, FormGroup, FormControl, Button } from 'react-bootstrap';
 import React, { Component } from 'react';
-
+import React, {useState} from 'react';
+import "./login.css"
 
 
 class Login extends Component {
@@ -11,81 +12,40 @@ class Login extends Component {
 			password: '',
 		};
 	}
-
-    render() {
-        console.log(this.state);
-        return (	
-//          <div >
-//          <Form horizontal className="LoginForm" id="loginForm">
-//          <FormGroup>
-//          <FormControl type="email" placeholder="Email Address" onChange = {(event) => this.setState ({email: event.target.value})}/>
-//          </FormGroup>
-//          <FormGroup>
-//          <FormControl type="password" placeholder="Password" onChange = {(event) => this.setState ({password: event.target.value})}/>
-//          </FormGroup>
-//          <FormGroup>
-//          <Button >
-//          Login
-//          </Button>
-//          
-//          </FormGroup>
-//          
-//          </Form>
-//          </div>
-            
-            
-            <div>
-            <meta charSet="UTF-8" />
-            <title>Purple Donkey</title>
-            <link rel="stylesheet" href="style.css" />
-            <div className="background">
-            <div className="form-box">
-            <div className="button-box">
-            <div id="btn">
-            </div>
-            <button type="button" className="toggle-btn" onclick="login()">Log In</button>
-            <button type="button" className="toggle-btn" onclick="register()">Register</button>
-            </div>
-            
-            <form id="login" className="input-group">
-            <input type="text" className="input-field" placeholder="User Id" required />
-            <input type="text" className="input-field" placeholder="Enter Password" required />
-            <input type="checkbox" className="check-box" /><span>Remember Password</span>
-            <button type="submit" className="submit-btn">Log In</button>
-            </form>
-    
-            <form id="register" className="input-group">
-            <input type="text" className="input-field" placeholder="User Id" required />
-            <input type="email" className="input-field" placeholder="Email Id" required />
-            <input type="text" className="input-field" placeholder="Enter Password" required />
-            <input type="checkbox" className="check-box" /><span>I agree to the terms and conditions</span>
-            <button type="submit" className="submit-btn">Register</button>
-            </form>
-            </div>
-            </div>
-            </div>
-            
-//          <script>
-//          var x = document.getElementById("login");
-//          var y = document.getElementById("register");
-//          var z = document.getElementById("btn");
-//          
-//          function register() {
-//              x.style.left = "-400px"
-//              y.style.left = "50px"
-//              z.style.left = "100px"
-//          }
-//          
-//          function login() {
-//              x.style.left = "50px"
-//              y.style.left = "450px"
-//              z.style.left = "0px"
-//          }
-//          </script>
-
-
-        );
-    }
 }
+
+const Login = () => {
+    
+    const [popupStyle, showPopup] = useState("hide")
+    
+    const popup = () => {
+        showPopup("login-popup")
+        setTimeout(() => showPopup("hide"), 3000)
+    }
+    
+    return (
+        <div className="background">
+        <div className="cover">
+            <h1>Login</h1>
+            <input type="text" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+            
+            <div className="login-btn" onClick={popup}>Login</div>
+            
+            
+            
+            <div className={popupStyle}>
+            <h3>Login Failed</h3>
+            <p>Username or password incorrect</p>
+            </div>
+            
+    
+        </div>
+        </div>
+        
+        
+    )
+}
+
 
 export default Login;
