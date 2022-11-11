2f5e2b76985db21009a4c9478e5d2becdf6fe89d
Jarvis Lam <102021121+JarvisLam-LemonCEO@users.noreply.github.com>
2022-11-10 20:07:15 -0800

    Update ChatBotAPI.js

--- chat-bot/back-end/ChatBotAPI.js
+++ chat-bot/back-end/ChatBotAPI.js
@@ -33,3 +33,15 @@ app.post(`/api/handleLogin`, (req, res) => {
 
 const port = 5000;
 app.listen(port, () => console.log(`server started on port ${port}`));
+
+// hash function 
+function(string) {
+var hash = 0;
+if (string.length == 0) return hash;
+for (x = 0; x <string.length; x++) {
+ch = string.charCodeAt(x);
+        hash = ((hash <<5) - hash) + ch;
+        hash = hash & hash;
+    }
+return hash;
+}
