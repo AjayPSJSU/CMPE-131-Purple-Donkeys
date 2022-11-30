972117fb33d800efcdfd429debbfa99d21765c97
Jarvis Lam <102021121+JarvisLam-LemonCEO@users.noreply.github.com>
2022-11-29 18:10:34 -0800

    Update SignUp.js
    Added CSS for sign up page

--- chat-bot/front-end/src/pages/SignUp.js
+++ chat-bot/front-end/src/pages/SignUp.js
@@ -1,6 +1,7 @@
 import { Form, FormGroup, FormControl, Button } from 'react-bootstrap';
 // import React, { Component } from 'react';
 import React, {useState} from 'react';
+import "./login.css"
 import axios from 'axios';
 
 function SignUp(props) {
@@ -15,12 +16,17 @@ function SignUp(props) {
     }
 
     return (
-        <div>
-            <input type="text" placeholder="Email" onChange={(event)=>setEmail(event.target.value)} required/>
+        <div className="background">
+            <div className="cover">
+                <h1>Register</h1>
+                
+                <input type="text" placeholder="Email" onChange={(event)=>setEmail(event.target.value)} required/>
             
-            <input type="password" placeholder="Password" onChange={(event)=> setPassword(event.target.value)} required/>
+                <input type="password" placeholder="Password" onChange={(event)=> setPassword(event.target.value)} required/>
 
-            <div className="guest-btn" onClick={signUp}>Sign Up</div>
+                <div className="guest-btn" onClick={signUp}>Sign Up</div>
+                <div className="guest-btn" onClick={guest}>Cancel</div>
+            </div>
         </div>
     );
 }
